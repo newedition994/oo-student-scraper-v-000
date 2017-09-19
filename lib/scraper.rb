3229c9ff355c7eee77ceb297bf543d4dca171310
Breyanna Vass <breyanna.bvass@gmail.com>
2017-09-19 01:54:18 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,10 @@ class Scraper
   def self.scrape_index_page(index_url)
     index_page = Nokogiri::HTML(open(index_url))
     #binding.pry
-    
+    index_page.css(".student-card").collect.with_index do |cards, index|
+      scraped_students = {}
+      scraped_students[:name] = index_page.css(".student-name")[index]
+    end
   end
 
   def self.scrape_profile_page(profile_url)
