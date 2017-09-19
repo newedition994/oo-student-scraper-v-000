1de953a2626bfceec3c164a41b6d5742f536f600
Breyanna Vass <breyanna.bvass@gmail.com>
2017-09-19 01:06:34 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,9 +4,8 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-
-    html = Nokogiri::HTML(open(index_url))
-
+    index_page = Nokogiri::HTML(open(index_url))
+    
   end
 
   def self.scrape_profile_page(profile_url)
