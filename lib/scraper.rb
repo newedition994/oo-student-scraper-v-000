01f6c6c37b7e09378fefdfc313596526bc852c7b
Breyanna Vass <breyanna.bvass@gmail.com>
2017-09-19 02:10:25 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,21 +17,22 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     student_hash = {}
-    profile_page = Nokogiri::HTML(open(profile_url))
-    profile_page.css(".social-icon-container a").each_with_index do |links, index|
-      if links["href"].include?("twitter")
-        student_hash[:twitter] = links["href"]
-      elsif links["href"].include?("linkedin")
-        student_hash[:linkedin] = links["href"]
-      elsif links["href"].include?("github")
-        student_hash[:github] = links["href"]
-      else links["href"].include?("blog")
-        student_hash[:blog] = links["href"]
-      end
+     profile_page = Nokogiri::HTML(open(profile_url))
+     profile_page.css(".social-icon-container a").each_with_index do |links, index|
+       if links["href"].include?("twitter")
+         student_hash[:twitter] = links["href"]
+       elsif links["href"].include?("linkedin")
+         student_hash[:linkedin] = links["href"]
+       elsif links["href"].include?("github")
+         student_hash[:github] = links["href"]
+       else links["href"].include?("blog")
+         student_hash[:blog] = links["href"]
+       end
     end
-    student_hash[:profile_quote] = profile_page.css(".profile_quote").text
-    student_hash[:bio] = profile_page.css(".description-holder p").text
-    student_hash
+ 
+   student_hash[:profile_quote] = profile_page.css(".profile-quote").text
+   student_hash[:bio] = profile_page.css(".description-holder p").text
+   student_hash
   end
 
 end
