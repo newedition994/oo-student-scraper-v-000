10229cc7653726f68b567396802b4d79d5ccfd8e
Breyanna Vass <breyanna.bvass@gmail.com>
2017-09-19 01:03:09 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,12 +5,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     index_page = Nokogiri::HTML(index_url)
-
-    projects = []
-
-    index_page.css("li.project.grid_4").each do |project|
-      title = project.css("h2.bbcard_blur")
-    end
+    
   end
 
   def self.scrape_profile_page(profile_url)
